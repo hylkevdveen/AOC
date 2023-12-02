da0bc7a1a798f1bfe979d6d8623b7be0003af421
hylkevanderveen <vanderveen@dataprovider.com>
2023-12-02 19:11:49 +0100

    Refactor to separate Part 1 and 2

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -9,82 +9,83 @@ import (
 )
 
 func main() {
+	fmt.Println(Part1())
+	fmt.Println(Part2())
+}
+
+func Part1() int {
 	sum := 0
 	for _, line := range aoc.InputLines() {
-		sum += Part2(line)
-	}
-	fmt.Println(sum)
-}
-
-func Part1(line string) int {
-	number := 0
-	str := []rune(line)
-	fmt.Printf("%s\n", line)
-	// First digit
-	for i := 0; i < len(str); i++ {
-		if unicode.IsDigit(str[i]) {
-			digit, _ := strconv.Atoi(string(str[i]))
-			fmt.Printf("First digit: %d\n", digit)
-			number += 10 * digit
-			break
-		}
-	}
-	// Last digit
-	for i := len(str) - 1; i >= 0; i-- {
-		if unicode.IsDigit(str[i]) {
-			digit, _ := strconv.Atoi(string(str[i]))
-			fmt.Printf("Last digit: %d\n", digit)
-			number += digit
-			break
-		}
-	}
-	return number
-}
-
-func Part2(line string) int {
-	number := 0
-	ucLine := []rune(line)
-	fmt.Printf("%s\n", line)
-	// First digit
-	for i := 0; i < len(line); i++ {
-		if unicode.IsDigit(ucLine[i]) {
-			digit, _ := strconv.Atoi(string(ucLine[i]))
-			fmt.Printf("First digit: %d\n", digit)
-			number += 10 * digit
-			break
-		}
-		found := false
-		for val, digit := range aoc.DigitStrings {
-			if strings.HasPrefix(line[i:], digit) {
-				fmt.Printf("First digit: %d\n", val)
-				number += 10 * (val)
-				found = true
+		str := []rune(line)
+		fmt.Printf("%s\n", line)
+		// First digit
+		for i := 0; i < len(str); i++ {
+			if unicode.IsDigit(str[i]) {
+				digit, _ := strconv.Atoi(string(str[i]))
+				fmt.Printf("First digit: %d\n", digit)
+				sum += 10 * digit
 				break
 			}
 		}
-		if found {
-			break
-		}
-	}
-	// Last digit
-	for i := len(line); i > 0; i-- {
-		if unicode.IsDigit(ucLine[i-1]) {
-			digit, _ := strconv.Atoi(string(ucLine[i-1]))
-			fmt.Printf("Last digit: %d\n", digit)
-			number += digit
-			break
-		}
-		found := false
-		for val, digit := range aoc.DigitStrings {
-			if strings.HasSuffix(line[:i], digit) {
-				fmt.Printf("Last digit: %d\n", val)
-				number += val
-				found = true
+		// Last digit
+		for i := len(str) - 1; i >= 0; i-- {
+			if unicode.IsDigit(str[i]) {
+				digit, _ := strconv.Atoi(string(str[i]))
+				fmt.Printf("Last digit: %d\n", digit)
+				sum += digit
 				break
 			}
 		}
-		if found {
-			break
+	}
+	return sum
+}
+
+func Part2() int {
+	number := 0
+	for _, line := range aoc.InputLines() {
+		ucLine := []rune(line)
+		fmt.Printf("%s\n", line)
+		// First digit
+		for i := 0; i < len(line); i++ {
+			if unicode.IsDigit(ucLine[i]) {
+				digit, _ := strconv.Atoi(string(ucLine[i]))
+				fmt.Printf("First digit: %d\n", digit)
+				number += 10 * digit
+				break
+			}
+			found := false
+			for val, digit := range aoc.DigitStrings {
+				if strings.HasPrefix(line[i:], digit) {
+					fmt.Printf("First digit: %d\n", val)
+					number += 10 * (val)
+					found = true
+					break
+				}
+			}
+			if found {
+				break
+			}
+		}
+		// Last digit
+		for i := len(line); i > 0; i-- {
+			if unicode.IsDigit(ucLine[i-1]) {
+				digit, _ := strconv.Atoi(string(ucLine[i-1]))
+				fmt.Printf("Last digit: %d\n", digit)
+				number += digit
+				break
+			}
+			found := false
+			for val, digit := range aoc.DigitStrings {
+				if strings.HasSuffix(line[:i], digit) {
+					fmt.Printf("Last digit: %d\n", val)
+					number += val
+					found = true
+					break
+				}
+			}
+			if found {
+				break
+			}
 		}
 	}
 	return number
