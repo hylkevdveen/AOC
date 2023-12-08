61a4535c520436c563621c96b13ecb2d14040e13
hylkevanderveen <vanderveen@dataprovider.com>
2023-12-08 20:08:39 +0100

    2023 day 8

--- 2023/day8/main.go
+++ 2023/day8/main.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"../../aoc"
-	n "./network"
+	"./network"
 	"fmt"
 )
 
 func main() {
-	network := n.ReadNetwork()
-	fmt.Println(Part1(network))
-	fmt.Println(Part2(network))
+	n := network.ReadNetwork()
+	fmt.Println(Part1(n))
+	fmt.Println(Part2(n))
 }
 
-func Part1(network *n.Network) int {
-	return network.StepsToEnd(func(node string) bool { return node == "ZZZ" })
+func Part1(n *network.Network) int {
+	return n.StepsToEnd(func(node string) bool { return node == "ZZZ" })
 }
 
-func Part2(network *n.Network) int {
-	ends := make([]int, 0, len(network.StartNodes))
-	for _, node := range network.StartNodes {
-		network.CurrentNode = node
-		network.Steps = 0
-		network.InstructionIndex = 0
-		ends = append(ends, network.StepsToEnd(func(node string) bool { return node[2] == 'Z' }))
+func Part2(n *network.Network) int {
+	ends := make([]int, 0, len(n.StartNodes))
+	for _, node := range n.StartNodes {
+		n.CurrentNode = node
+		n.Steps = 0
+		n.InstructionIndex = 0
+		ends = append(ends, n.StepsToEnd(func(node string) bool { return node[2] == 'Z' }))
 	}
 	return aoc.LCM(ends[0], ends[1], ends[2:]...)
 }
