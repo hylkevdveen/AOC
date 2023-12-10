169a76461c129540191dd51040d341025247cd3e
hylkevanderveen <vanderveen@dataprovider.com>
2023-12-10 18:50:58 +0100

    Add check for template

--- README.md
+++ README.md
@@ -14,11 +14,13 @@ You can use [`startday.sh`](startday.sh) to set up the template script for your 
 
 Because you must be logged in to get your input, you need to get your Advent of Code session ID and set the `$AOC_SESSION` environment variable.
 The script will try to read it from the `.env` file if it exists so you can store it there.
-You can find your session ID by visiting the input page of any one challenge, opening the Network tab on your browsers inspection tool and looking for the Cookie field under Request Headers.
+
+You can find your session ID by visiting the input page of any one challenge, opening the Network tab on your browsers inspection tool and looking for the Cookie field under Request Headers on the input request.
 
 ### Code templates
 
 Add a folder [template](./template) in which you store the templates to your preferred language as plaintext files using just the scripts file extension as name (e.g. `template/ipynb`). 
+
 You might have to add this language to the script if it's not already included.
 
 _Happy coding!_
--- startday.sh
+++ startday.sh
@@ -7,6 +7,8 @@ day=$1
 [[ "$day" =~ ^[0-9]{1,2}$ && $day -ge 1 && $day -le 31 ]] || { echo "'$day' is not a valid day"; usage; }
 shift
 
+# language
+
 language=""
 
 while getopts "l:" opt; do
@@ -20,13 +22,10 @@ case $language in
   "go" ) echo "Setting up day for Go" ;;
   "ipynb" ) echo "Setting up day for Jupyter Notebook" ;;
   "" ) { echo "No language chosen, proceeding with Go"; language="go"; } ;;
-  * ) { echo "'$language' is not a valid programming language. If you have a template for it, add it to the case in this script."; usage; }
+  * ) { echo "'$language' is not a valid programming language. If you have a template for it, add the language to the case in this script."; usage; }
 esac
 
-aoc="https://adventofcode.com/$year/day/$day"
-echo "Preparing Advent of Code $year day $day: $aoc"
-
-dir="$year/day$day"
+# Session cookie
 
 if [ -f .env ]; then
   export "$(xargs < .env)"
@@ -37,11 +36,20 @@ if [ "$AOC_SESSION" = "" ]; then
   exit 1
 fi
 
+# creating directory
+
+aoc="https://adventofcode.com/$year/day/$day"
+echo "Preparing Advent of Code $year day $day: $aoc"
+
+dir="$year/day$day"
+
 if test -d "$dir"; then
   echo "Directory ./$dir already exists"
   exit 0
 fi
 mkdir -p "$dir"
+
+# input.txt
 
 echo "Fetching $aoc/input"
 
@@ -52,6 +60,13 @@ curl -fsS --request GET "$aoc/input" -H "cookie: session=$AOC_SESSION" -o "$dir/
 }
 
 echo "Created $dir/index.txt"
+
+# main script
+
+if ! test -f "template/$language"; then
+  echo "Template template/$language does not exist"
+  exit 1
+fi
 
 cp "template/$language" "$dir/main.$language"
 echo "Created $dir/main.$language"
