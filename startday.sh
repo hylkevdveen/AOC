d31cba34a2490c235caabea35c6d1af81d6d25a3
hylkevanderveen <vanderveen@dataprovider.com>
2023-12-10 15:19:02 +0100

    Update setup script

--- startday.sh
+++ startday.sh
@@ -1,26 +1,40 @@
-year=$1
-day=$2
-language=$3
-[[ "$year" =~ ^[0-9]{4}$ && $year -ge 2015 && $year -le $(date +%Y) ]] || { echo "'$year' is not a valid year"; exit 1; }
-[[ "$day" =~ ^[0-9]{1,2}$ && $day -ge 1 && $day -le 31 ]] || { echo "'$day' is not a valid day"; exit 1; }
+usage() { echo "Usage: $0 <yyyy> <d> [-l <go|ipynb>]" 1>&2; exit 1; }
 
+year=$1
+[[ "$year" =~ ^[0-9]{4}$ && $year -ge 2015 && $year -le $(date +%Y) ]] || { echo "'$year' is not a valid year"; usage; }
+shift
+day=$1
+[[ "$day" =~ ^[0-9]{1,2}$ && $day -ge 1 && $day -le 31 ]] || { echo "'$day' is not a valid day"; usage; }
+shift
+
+aoc="https://adventofcode.com/$year/day/$day"
+echo "Preparing Advent of Code $year day $day: $aoc"
+
+language=""
+
+while getopts "l:" opt; do
+  case "$opt" in
+    "l" ) language=${OPTARG} ;;
+    * ) usage
+  esac
+done
 
 case $language in
   "go" ) echo "Setting up day for Go" ;;
   "ipynb" ) echo "Setting up day for Jupyter Notebook" ;;
   "" ) { echo "No language chosen, proceeding with Go"; language="go"; } ;;
-  * ) { echo "'$language' is not a valid programming language. If you have a template for it, add it to the case in this script."; exit 1; }
+  * ) { echo "'$language' is not a valid programming language. If you have a template for it, add it to the case in this script."; usage; }
 esac
 
 dir="$year/day$day"
-aoc="https://adventofcode.com/$year/day/$day/input"
 
-if [ ! -f .env ]; then
-  export "$(xargs < .env)"
+if [ -f .env ]; then
+  export $(xargs < .env)
 fi
 
 if [ "$AOC_SESSION" = "" ]; then
   echo "Set the 'AOC_SESSION' environment variable to your Advent of Code session cookie"
+  exit 1
 fi
 
 if test -d "$dir"; then
@@ -29,9 +43,9 @@ if test -d "$dir"; then
 fi
 mkdir -p "$dir"
 
-echo "Fetching $aoc"
+echo "Fetching $aoc/input"
 
-curl -fsS --request GET "$aoc" -H "cookie: session=$AOC_SESSION" -o "$dir/input.txt" || {
+curl -fsS --request GET "$aoc/input" -H "cookie: session=$AOC_SESSION" -o "$dir/input.txt" || {
   echo "It's possibly too soon access this day";
   rmdir "$dir";
   exit 1;
